86be635d72444e23cd84673cd200c02a0348a9dd
Filip Dimitrov <github@filip-dimitrov.eu>
2022-06-13 21:01:19 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TOKEN=$(grep token ~/.terraformrc | cut -f 2 -d \\\"
+TOKEN=$(grep token ~/.terraformrc | cut -f 2 -d \\\")
 echo $TOKEN 1>&2
 curl --header "Authorization: Bearer $TOKEN" --header "Content-Type: application/vnd.api+json" https://app.terraform.io/api/v2/workspaces/ws-eMs8keix7KspYjZW/current-state-version-outputs | jq . 1>&2
 exit 1

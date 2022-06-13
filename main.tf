8c3f5d24d0205f36cf332bb3b2f43ac56c9c6633
Filip Dimitrov <github@filip-dimitrov.eu>
2022-06-13 16:18:48 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+/*
+resource "null_resource" "test" {
+  
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = ""
+    #command = "docker build -t ${var.image_name}:${var.image_tag} ./path-to-docker-file-Folder"
+    }
+}
+*/
+  
+data "tfe_outputs" "one" {
+  organization = "ticket74636"
+  workspace    = "ticket74636-one"
+}
+
+output "one" {
+  #    value = nonsensitive(data.tfe_outputs.network.values)
+  value = nonsensitive(data.tfe_outputs.one.values)
+}
